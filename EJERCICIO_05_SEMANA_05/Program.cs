e5a9489db8237a3edc188c93775495a591140e3e
Jorge Mendosa <jl.mendosam@uea.edu.ec>
2025-06-30 00:02:13 -0500

    EJERCICIO_05_SEMANA_05

--- EJERCICIO_05_SEMANA_05/Program.cs
+++ EJERCICIO_05_SEMANA_05/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace EJERCICIO_05_SEMANA_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // hacer una lista
+            List<int> numeros = new List<int>();
+            
+            // meter los numeros del 1 al 10
+            for (int i = 1; i <= 10; i++)
+            {
+                numeros.Add(i);
+            }
+            
+            // voltear la lista
+            numeros.Reverse();
+            
+            // imprimir separado por comas
+            for (int i = 0; i < numeros.Count; i++)
+            {
+                Console.Write(numeros[i]);
+                if (i < numeros.Count - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+    }
+}
+
